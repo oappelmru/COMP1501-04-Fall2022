b2aabb57de9318aae0ff64af4129709ea534e4a3
Orestes Appel <110573210+oappelmru@users.noreply.github.com>
2022-09-22 17:41:53 -0600

    Delete C1501Fall2022/graded directory

--- C1501Fall2022/graded/fstrings/fstring.py
+++
@@ -1,9 +0,0 @@
-GST = 0.05
-
-amount = float(input("Enter the pre-tax total of your order: "))
-total = amount * (1 + GST)
-
-# Modify the following line to display the total with exactly 2 decimal places.
-# Include a comment citing your source!
-print("Before tax: $", amount)
-print("Total:      $", total)
